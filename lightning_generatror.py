043cb795c11a67810038cc05b88f44fb5c4a641a
Xinyi Chen <xinyichencxy@gmail.com>
2018-01-06 13:23:35 -0800

    draw on background directly

--- lightning_generatror.py
+++ lightning_generatror.py
@@ -1,11 +1,8 @@
 from PIL import Image, ImageDraw
 
 background = Image.open("rainy_sky.jpg")
-foreground = Image.new('RGB', (400, 400))
-
-draw = ImageDraw.Draw(foreground)
+draw = ImageDraw.Draw(background)
 draw.line((100, 200, 150, 300), fill=128)
 
-background.paste(foreground, (0, 0))
 background.show()
 background.save('test.png', 'PNG')
