e1a3c4509a1b3a7a8eac807aefcc8fe6f3450b05
Xinyi Chen <xinyichencxy@gmail.com>
2018-01-06 13:06:58 -0800

    basic img overlay

--- lightning_generatror.py
+++ lightning_generatror.py
@@ -1,7 +1,11 @@
 from PIL import Image, ImageDraw
 
-im = Image.new('RGBA', (400, 400), (0, 255, 0, 0))
-draw = ImageDraw.Draw(im)
-draw.line((100,200, 150,300), fill=128)
-im.show()
-im.save('test.png', 'PNG')
+background = Image.open("rainy_sky.jpg")
+foreground = Image.new('RGB', (400, 400))
+
+draw = ImageDraw.Draw(foreground)
+draw.line((100, 200, 150, 300), fill=128)
+
+background.paste(foreground, (0, 0))
+background.show()
+background.save('test.png', 'PNG')
